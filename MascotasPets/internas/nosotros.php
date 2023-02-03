5c5199d0d2ab2fcec0162983c584d8eb315380f7
Dennis Jiménez <dennisleonardojimenezdh@gmail.com>
2023-02-03 08:25:32 -0500

    estilos

--- MascotasPets/internas/nosotros.php
+++ MascotasPets/internas/nosotros.php
@@ -3,12 +3,13 @@
 ?>
     
 <body>
-    <div class ="contenedor" id="columnas">
+    <div class ="contenedor colorBlanco" id="columnas">
     <section class="imgNosotros">
-		<img src="#" title="">
+		<img src="../img/quienesSomos.png" title="">
 	</section>
-        <section>
-            <h2>¿Quienes Somos?</h2>
+        <section class="container-all1">
+            <img src="../img/adornomascota.png" title="">
+            <h2 class= "sub-title">¿Quienes Somos?</h2>
             <p>
             La fundación Protección Animal Ecuador, PAE, se fundó y legalizó en Quito el 3 de agosto de 1984 
             ante el Ministerio de Inclusión Social y Económica bajo la figura original de asociación 
@@ -19,14 +20,14 @@
             </p>
         </section>
 
-        <section>
+        <section class="container-all2">
             <h3>Misión:</h3>
             <p>
             Promovemos la protección y el bienestar de los animales mediante acciones directas 
             y la concienciación de la comunidad en el respeto que merecen y se debe tener hacia las demás especies.
             </p>
         </section>
-        <section>
+        <section class="container-all3">
             <h3>Visión:</h3>
             <p>
             Ser una organización autosustentable, reconocida como referente nacional en bienestar animal, 
